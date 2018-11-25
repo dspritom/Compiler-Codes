7a8a2eccbce4293e9e7217ed9bcc249ce0a3f4a0
Deepto Shome Pritom <diptoshome45@gmail.com>
2018-11-25 23:53:51 +0600

    Add files via upload

---
+++ LeftRecursion.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    string productions;
+    cin >> productions;
+
+    vector<string> recursive, non_recursive;
+
+    string temp = "";
+    char start_var = productions[0];
+
+    for (int i = 3; i < productions.length(); i++)
+    {
+        if (productions[i] == '|')
+        {
+            if (temp[0] == start_var)
+            {
+                recursive.push_back(temp);
+                temp = "";
+            }
+            else
+            {
+                non_recursive.push_back(temp);
+                temp = "";
+            }
+        }
+        else
+        {
+            temp += productions[i];
+        }
+    }
+    if (temp != "")
+    {
+        if (temp[0] == start_var)
+        {
+            recursive.push_back(temp);
+            temp = "";
+        }
+        else
+        {
+            non_recursive.push_back(temp);
+            temp = "";
+        }
+    }
+    if (temp == "empty")
+    {
+        non_recursive.push_back(temp);
+        temp = "";
+    }
+
+    cout << start_var << "->";
+    for (int i = 0; i < non_recursive.size()-1; i++)
+    {
+        cout << non_recursive[i] << start_var << "'" << "|";
+    }
+    cout << non_recursive[non_recursive.size()-1] << start_var << "'" << endl;
+    cout << start_var << "'->";
+    for (int i = 0; i < recursive.size(); i++)
+    {
+        string alpha = "";
+        for (int j = 1; j < recursive[i].size(); j++)
+        {
+            alpha += recursive[i][j];
+        }
+
+        cout << alpha << start_var << "'|";
+    }
+    cout << "empty" << endl;
+
+    return 0;
+}
